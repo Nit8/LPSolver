d160e19017b72c367554c49ba90dc22d10925c2c
Nitesh Jaiswal <nitesh.singh1423.nl@gmail.com>
2025-05-08 02:21:50 +0545

    updated setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="lpsolver",
-    version="0.1",
+    version="0.1.2",
+    description="A Python Linear Programming Solver with Xpress-like syntax",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Your Name",
+    author_email="nitesh.singh1423.nl@gmail.com",
     packages=find_packages(),
-    install_requires=[
-        'numpy',
+    install_requires=["numpy>=1.20.0"],
+    python_requires=">=3.8",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
 )
